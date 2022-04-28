80dc046b235a454fe2a393e213e5683afe6069c4
2320sharon <sharonfitzpatrick@csus.edu>
2022-04-28 10:11:09 -0700

    csv file not created until download clicked

--- sniffer.py
+++ sniffer.py
@@ -28,17 +28,12 @@ def create_csv(csv_filename=None):
         d1 = today.strftime("%d_%m_%Y_hr_%H_%M")
         filename = f"Sniffer_Output_" + d1 + ".csv"
         csv_path = filename
-    with open(csv_path, 'w', newline='') as outcsv:
-        writer = csv.writer(outcsv)
-        writer.writerow(["Filename", "Sorted", "index"])
+    st.session_state.df.to_csv(csv_filename)
     return csv_path
 
 def yes_button():
     if -1 < st.session_state.img_idx < len(images_list)-1:
-        if st.session_state.csv_file is None: 
-            st.session_state.csv_file=create_csv()
         row={"Filename":images_list[st.session_state.img_idx],'Sorted':"good",'Index':st.session_state.img_idx}
-        # st.session_state.df=st.session_state.df.append(row,ignore_index=True)
         st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
         with open(st.session_state.csv_file, 'a', newline='') as outcsv:
             writer = csv.writer(outcsv)
@@ -52,10 +47,7 @@ def yes_button():
 
 def no_button():
     if -1 < st.session_state.img_idx < len(images_list)-1:
-        if st.session_state.csv_file is None: 
-            st.session_state.csv_file=create_csv()
         row={"Filename":images_list[st.session_state.img_idx],'Sorted':"bad",'Index':st.session_state.img_idx}
-        # st.session_state.df=st.session_state.df.append(row,ignore_index=True)
         st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
         with open(st.session_state.csv_file, 'a', newline='') as outcsv:
             writer = csv.writer(outcsv)
@@ -71,7 +63,6 @@ def undo_button():
     if st.session_state.img_idx >0:
         st.session_state.img_idx -= 1
         drop_filename=images_list[st.session_state.img_idx]
-        print(f"drop_filename: {drop_filename}")
         index=st.session_state.df.loc[st.session_state.df['Filename'] == drop_filename].index.values
         st.session_state.df.drop(index, axis=0, inplace=True)
     else:
@@ -90,7 +81,7 @@ col1,col2,col3=st.columns(3)
 with col1:
     st.button(label="Yes",key="yes_button",on_click=yes_button)
     st.button(label="No",key="no_button",on_click=no_button)
-    st.button(label="Undo",help="Undo the last sort",key="undo_button",on_click=undo_button)
+    st.button(label="Undo",key="undo_button",on_click=undo_button)
     
 with col2:
     st.image(image, caption=f'Image #{st.session_state.img_idx}',width=300)
